0717a30c2831f90e528142267f86b9d1c90f4b56
Vitalii Boiko <vitaliy.boiko@playrix.study>
2022-02-09 10:02:39 +0200

    Squashed commit of the following:
    commit 31c5b2ff25677a40c68014b47da65b2454499738
    Author: pasha-str27 <program.pasha@gmail.com>
    Date:   Wed Feb 9 09:43:23 2022 +0200
    
        refactored ball moving script
    
    commit b8d65341643960239c8363f68e47252af5a30b55
    Author: pasha-str27 <program.pasha@gmail.com>
    Date:   Tue Feb 8 10:07:27 2022 +0200
    
        Squashed commit of the following:
    
        commit 29abbc051e69e2c38965826de19fc771406ff280
        Author: Vitalii Boiko <vitaliy.boiko@playrix.study>
        Date:   Mon Feb 7 12:37:30 2022 +0200
    
            added endgame panel to hud
    
        commit 8bf94a372e3dff1635849c729a76e9dc5bd98030
        Author: Vitalii Boiko <vitaliy.boiko@playrix.study>
        Date:   Mon Feb 7 11:25:24 2022 +0200
    
            fixed UI in main menu
    
        commit ba373f195de38c317f06187deb9e7d26f396385d
        Author: Vitalii Boiko <vitaliy.boiko@playrix.study>
        Date:   Sat Feb 5 21:10:49 2022 +0200
    
            upgraded ScrollRect in pause menu
    
        commit 50c57e434210c6b2761b331c3c540102e2b04a5d
        Author: Vitalii Boiko <vitaliy.boiko@playrix.study>
        Date:   Sat Feb 5 17:07:34 2022 +0200
    
            added ingame hud
    
        commit ccb8868b91f93b51d4b8d3c3f97807928d2ad2b9
        Author: Vitalii Boiko <vitaliy.boiko@playrix.study>
        Date:   Sat Feb 5 14:55:11 2022 +0200
    
            added shop and some functionality to main menu ui
    
    commit 6ee3d36f2daa04b028f81f36b6eb0cdbee34cbdb
    Author: pasha-str27 <program.pasha@gmail.com>
    Date:   Thu Feb 3 11:52:40 2022 +0200
    
        added gamemanager and simple stickmanbehaviour
    
    commit 1265184eb87d84be1e01c7486188f976397e6b18
    Author: pasha-str27 <program.pasha@gmail.com>
    Date:   Thu Feb 3 11:09:41 2022 +0200
    
        added walls position changer according to screen size
    
    commit 9d71e2640388230e8621c9c0c4cf9a366782f833
    Author: pasha-str27 <program.pasha@gmail.com>
    Date:   Thu Feb 3 10:59:54 2022 +0200
    
        added 3d physics to walls and ball
    
    commit 9f305f3f767d8499e78463af067b7b93c105cdb1
    Merge: c4630bf 47d9a6a
    Author: pasha-str27 <program.pasha@gmail.com>
    Date:   Thu Feb 3 10:19:23 2022 +0200
    
        Merge branch 'vitalik' into pasha
    
    commit c4630bffa5263f63badb02ae298ab7a8f286306d
    Author: pasha-str27 <program.pasha@gmail.com>
    Date:   Wed Feb 2 12:12:05 2022 +0200
    
        fixed some bugs
    
    commit a450ac773f8dbc1cc3aa6c9ef43f5536bdd257c4
    Author: pasha-str27 <program.pasha@gmail.com>
    Date:   Wed Feb 2 11:47:53 2022 +0200
    
        added ball moving

---
+++ Assets/Scripts/BallMoving.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMoving : MonoBehaviour
+{
+    [SerializeField] float speedCoeficient = 0.01f;
+    private Transform ballTransform;
+    private Rigidbody rigidbody;
+
+    Vector2 startPosition;
+
+    void Start()
+    {
+        GameManager.Instance().Reset();
+        ballTransform = gameObject.transform;
+        startPosition = ballTransform.position;
+
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void SetMovingParameters(Vector2 direction, float speed)
+    {
+        if (rigidbody.velocity.magnitude > 0)
+            return;
+
+        rigidbody.velocity = direction * speed * speedCoeficient;
+
+        print(direction * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("DownWall"))
+        {
+            ballTransform.position = startPosition;
+
+            rigidbody.velocity = Vector2.zero;
+
+            return;
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine.Events;
+
+class GameManager
+{
+    static GameManager instance;
+
+    int scoreCount = 0;
+
+    UnityEvent onScoreChanged;
+
+    public static GameManager Instance()
+    {
+        instance = instance ?? new GameManager();
+
+        return instance;
+    }
+
+    public void SubscribeOnScoreChanged(UnityAction action)
+    {
+        if (onScoreChanged == null)
+            onScoreChanged = new UnityEvent();
+
+        onScoreChanged.AddListener(action);
+    }
+
+    public void ResetOnScoreChanged()
+    {
+        if (onScoreChanged == null)
+            onScoreChanged = new UnityEvent();
+
+        onScoreChanged.RemoveAllListeners();
+    }
+
+    public void AddScore(int scores)
+    {
+        scoreCount += scores;
+
+        onScoreChanged?.Invoke();
+    }
+
+    public int GetScoreCount()
+    {
+        return scoreCount;
+    }
+
+    public void Reset()
+    {
+        scoreCount = 0;
+        ResetOnScoreChanged();
+    }
+}
